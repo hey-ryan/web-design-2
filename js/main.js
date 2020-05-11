ad5938f6ef7e09d4b927c6e5fd86d14197812c8e
Ryan Carranco <rjoncarranco@gmail.com>
2020-05-10 20:41:25 -0500

    added Scroll function

--- js/main.js
+++ js/main.js
@@ -1,6 +1,30 @@
 $(document).ready(function () {
 
-    $('#covidUpdate').click(function (){
+    var scrollLink = $('.scroll');
+
+    // Smooth scrolling
+    scrollLink.click(function(e) {
+        e.preventDefault();
+        $('body,html').animate({
+            scrollTop: $(this.hash).offset().top}, 1000 );
+    });
+
+    // Active link switching
+    $(window).scroll(function() {
+        var scrollbarLocation = $(this).scrollTop();
+
+        scrollLink.each(function() {
+
+            var sectionOffset = $(this.hash).offset().top - 160;
+
+            if ( sectionOffset <= scrollbarLocation ) {
+                $(this).parent().addClass('active');
+                $(this).parent().siblings().removeClass('active');
+            }
+        })
+    })
+
+    /* $('#covidUpdate').click(function (){
 
             $('#protocol').addClass('covidStyle');
             $('#button-container').hide(2000);
@@ -8,7 +32,7 @@ $(document).ready(function () {
 
     setTimeout(function(){
         $('#dismiss').hide();
-    }, 3000);
+    }, 3000); */
 
 });
 
